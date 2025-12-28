735764d005b5a29311203c30e888940f456b4341
Vivek Jha <progamervivek2020@gmail.com>
2025-12-28 14:43:34 +0530

    Refactor Section01 component: update text sizes and enhance layout for improved design and readability

--- src/components/Section01.jsx
+++ src/components/Section01.jsx
@@ -15,17 +15,17 @@ function Section01() {
       </div>
       <div className="w-full rounded-3xl bg m-1 h-160 border-1 overflow-hidden border-black">
         <div className="w-full h-full pr-10 pl-10">
-          <div className="h-2/7 w-full border-b-1 flex justify-center  text-[11vw] lobster border-black ">
+          <div className="h-2/7 w-full border-b-1 flex justify-center  text-[12vw] lobster border-black ">
             I
           </div>
-          <div className="h-2/7 w-full border-b-1 flex justify-between items-center text-[11vw] lobster border-black ">
+          <div className="h-2/7 w-full border-b-1 flex justify-between items-center text-[12vw] lobster border-black ">
             <p className="w-2/7 indent-45 text-xl font-normal">
               I build clean, creative web experiences and evolving into a
               MERN-stack developer with AI-powered features.
             </p>
             <p className="w-3/5 pr-40 flex justify-end text-[#ff3d00]">AM</p>
           </div>
-          <div className="h-2/7 text-[#ff3d00] w-full border-b-1 flex justify-start text-[11vw] lobster border-black ">
+          <div className="h-2/7 text-[#ff3d00] w-full border-b-1 flex justify-start text-[12vw] lobster border-black ">
             Developer
           </div>
           <div className="h-1/7 w-full flex justify-between text-[#ff3d00] p-3 lobster ">
@@ -36,8 +36,103 @@ function Section01() {
           </div>
         </div>
       </div>
-      <div className="w-full rounded-3xl bg-[#ff3d00] m-1 h-160 "></div>
-      <div className="w-full rounded-3xl bg m-1 h-120 border-1 border-black"></div>
+      <div className="w-full lobster rounded-3xl bg-[#ff3d00] m-1 h-350 overflow-hidden ">
+        <div className="w-full h-full pl-6 pr-6 text-white">
+          <div className="h-1/27 w-full text-[5vw]"></div>
+          <div className="h-2/27 w-full border-b-1 flex justify-between border-black text-[6vw]">
+            <div></div>
+            <div className="flex gap-23 items-center">
+              <p className="text-xl text-black border-1 border-black p-2 rounded-full">
+                000
+              </p>
+              <p>Product design should feel</p>
+            </div>
+          </div>
+          <div className="h-2/27 w-full border-b-1 flex justify-center  border-black text-[6vw]">
+            creative, intentional, and driven by
+          </div>
+          <div className="h-2/27 w-full flex justify-start  text-[6vw]">
+            real user experiences.
+          </div>
+          <div className="h-2/27 w-full text-[5vw]"></div>
+          <div className="h-2/27 w-full border-b-1 flex justify-evenly items-center border-black ">
+            <p className="text-xl text-black border-1 border-black p-2 rounded-full">
+              001
+            </p>
+            <p className="text-[6vw] ">Over-optimization kills</p>
+          </div>
+          <div className="h-2/27 w-full border-b-1 border-black text-[6vw]">
+            originality;meaningful innovation
+          </div>
+          <div className="h-2/27 w-full border-b-1 flex justify-start border-black text-[6vw]">
+            comes from experimentation,not
+          </div>
+          <div className="h-2/27 w-full flex justify-start  text-[6vw]">
+            playing safe.
+          </div>
+          <div className="h-2/27 w-full text-[5vw]"></div>
+          <div className="h-2/27 w-full border-b-1 flex justify-evenly items-center border-black ">
+            <p className="text-xl text-black border-1 border-black p-2 rounded-full">
+              010
+            </p>
+            <p className="text-[6vw]">When everything looks</p>
+          </div>
+          <div className="h-2/27 w-full border-b-1 border-black text-[6vw]">
+            the same, creative differentiation
+          </div>
+          <div className="h-2/27 w-full text-[6vw]">
+            becomes the strongest advantage.
+          </div>
+          <div className="h-2/27 w-full flex justify-between text-[#ff3d00] p-3 lobster ">
+            <p className="text-white">(Hello)</p>
+            <p className="text-black">
+              <span className="mb-[2px] mr-[3px]">●</span>001/011
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="w-full rounded-3xl bg m-1 h-250 p-6 border-1 border-black">
+        <div className="text-[6vw] lobster h-1/16"></div>
+        <div className=" lobster flex justify-between items-center h-2/23">
+          <p className="text-3xl flex justify-start">My Mission</p>
+          <p className="text-[6vw]">Building</p>
+        </div>
+        <div className="text-[6vw] flex justify-end items-center lobster h-2/23">
+          <span className="text-[#ff3d00]">creative foundations</span>
+        </div>
+        <div className="text-[6vw] lobster flex justify-end h-1/16 ">
+          <div className="text-[6vw] w-4/5 lobster border-b-1 border-black"></div>
+        </div>
+        <div className="text-[6vw] lobster h-1/16"></div>
+        <div className=" lobster flex justify-between items-center h-2/23">
+          <p className="text-3xl flex justify-start">Challenge</p>
+          <p className="text-[6vw]">breaking</p>
+        </div>
+        <div className="text-[6vw] flex justify-end items-center lobster h-2/23">
+          <span className="text-[#ff3d00]">repetitive design&nbsp;</span>{" "}
+          thinking
+        </div>
+        <div className="text-[6vw] lobster flex justify-end h-1/16 ">
+          <div className="text-[6vw] w-4/5 lobster border-b-1 border-black"></div>
+        </div>
+        <div className="text-[6vw] lobster h-1/16"></div>
+        <div className=" lobster flex justify-between items-center h-2/23">
+          <p className="text-3xl flex justify-start">Goal</p>
+          <p className="text-[6vw]">
+            and shaping&nbsp;<span className="text-[#ff3d00]">AI-powered,</span>
+          </p>
+        </div>
+        <div className="text-[6vw] flex justify-end items-center lobster h-2/23">
+          future-ready&nbsp;
+          <span className="text-[#ff3d00]">digital experiences.</span>
+        </div>
+        <div className="h-1/16 mt-10 w-full flex justify-between text-[#ff3d00] p-3 lobster ">
+          <p className="text-[#ff3d00]">(Hello)</p>
+          <p className="text-black">
+            <span className="mb-[2px] mr-[3px]">●</span>001/011
+          </p>
+        </div>
+      </div>
       <div className="w-full rounded-3xl bg-[#ff3d00] m-1 h-120 "></div>
       <div className="w-full rounded-3xl bg m-1 h-120 border-1 border-black"></div>
       <div className="w-full rounded-3xl bg-[#ff3d00] m-1 h-120 "></div>
