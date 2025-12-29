03e7c5b5ec2ce44b723563b402e84bb58e6f2788
Vivek Jha <progamervivek2020@gmail.com>
2025-12-29 16:37:08 +0530

    Fix LeftSection component: update sticky behavior for improved layout

--- src/components/LeftSection.jsx
+++ src/components/LeftSection.jsx
@@ -17,8 +17,8 @@ const Section = [
 ];
 function LeftSection() {
   return (
-    <div className="md:w-2/10 md:h-screen h-1/11 sticky top-0 md:p-5 no-scrollbar overflow-x-auto md:overflow-y-auto">
-      <div className="flex md:flex-col flex-row ">
+    <div className="w-2/10 md:h-screen h-1/11 md:p-5 no-scrollbar overflow-x-auto md:overflow-y-auto">
+      <div className="flex w-full md:sticky fixed bottom-0 overflow-x-auto md:flex-col flex-row ">
         {Section.map((SectionItem, index) => (
           <div
             key={index}
