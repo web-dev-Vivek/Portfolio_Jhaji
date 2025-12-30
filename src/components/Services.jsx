0c4312e3ef03095a3654a74d981dfef77552e398
Vivek Jha <progamervivek2020@gmail.com>
2025-12-30 22:10:20 +0530

    Enhance Services component: update text styles and add service categories

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -25,22 +25,67 @@ function Services() {
       </div>
       <div className="w-full flex flex-col gap-2 justify-between rounded-3xl m-1 md:h-100 ">
         <div className="rounded-3xl flex justify-between  p-5 items-center bg-[#ff003D] w-full h-2/6">
-          <p className="w-1/7 text-black">(Clients)</p>
-          <p className="w-5/7 text-black text-7xl lobster">Service I</p>
-          <p className="w-1/7 text-black  flex justify-end">(Partners)</p>
+          <p className="w-1/7 text-black font-bold">(Clients)</p>
+          <p className="w-5/7 text-white text-8xl lobster flex justify-center">
+            Services
+          </p>
+          <p className="w-1/7 text-black  flex justify-end font-bold">
+            (Partners)
+          </p>
         </div>
         <div className="rounded-3xl flex justify-between p-5  items-center bg-[#ff003D] w-full h-2/6">
-          <p className="w-1/7 text-black ">(Clients)</p>
-          <p className="w-5/7 text-black text-7xl lobster"></p>
-          <p className="w-1/7 text-black flex justify-end">(Partners)</p>
+          <p className="w-1/7 text-black font-bold">(Clients)</p>
+          <p className="w-5/7 text-white text-8xl lobster flex justify-center">
+            Provided To
+          </p>
+          <p className="w-1/7 text-black flex justify-end font-bold">
+            (Partners)
+          </p>
         </div>
         <div className="rounded-3xl flex justify-between p-5  items-center bg-[#ff003D] w-full h-2/6">
-          <p className="w-1/7 text-black">(Clients)</p>
-          <p className="w-5/7 text-black text-7xl lobster"></p>
-          <p className="w-1/7 text-black flex justify-end">(Partners)</p>
+          <p className="w-1/7 text-black font-bold">(Clients)</p>
+          <p className="w-5/7 text-white text-8xl lobster flex justify-center">
+            Our Partner
+          </p>
+          <p className="w-1/7 text-black flex justify-end font-bold">
+            (Partners)
+          </p>
         </div>
       </div>
-      <div></div>
+      <div className="flex flex-col rounded-3xl overflow-hidden m-1 border-1 border-black mt-5 h-100 w-full">
+        <div className="w-full h-2/15 flex flex-row ">
+          <div className="border-1 border-black flex justify-center items-center font-bold text-lg w-1/4 h-full">
+            Website Design
+          </div>
+          <div className="border-1 border-black flex justify-center items-center font-bold text-lg w-1/4 h-full">
+            Mobile App Design
+          </div>
+          <div className="border-1 border-black flex justify-center items-center font-bold text-lg w-1/4 h-full">
+            Logo Design
+          </div>
+          <div className="border-1 border-black flex justify-center items-center font-bold text-lg w-1/4 h-full">
+            Framer devlopment
+          </div>
+        </div>
+        <div className="w-full h-13/15"></div>
+      </div>
+      <div className="flex flex-col rounded-3xl overflow-hidden m-1 border-1 border-black h-100 w-full">
+        <div className="w-full h-2/15 flex flex-row ">
+          <div className="border-1 border-black flex justify-center items-center font-bold text-lg w-1/4 h-full">
+            Dashboard Design
+          </div>
+          <div className="border-1 border-black flex justify-center items-center font-bold text-lg w-1/4 h-full">
+            Website Development
+          </div>
+          <div className="border-1 border-black flex justify-center items-center font-bold text-lg w-1/4 h-full">
+            Frontend Development
+          </div>
+          <div className="border-1 border-black flex justify-center items-center font-bold text-lg w-1/4 h-full">
+            Backend Development
+          </div>
+        </div>
+        <div className="w-full h-13/15"></div>
+      </div>
     </div>
   );
 }
